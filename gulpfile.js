a9af462f79d0b53d7b28bc4226221acdc4ef808f
Bruno Basto <bruno.basto@liferay.com>
2017-03-10 17:13:22 -0300

    Test

--- gulpfile.js
+++ gulpfile.js
@@ -20,48 +20,48 @@ metal.registerTasks({
 	noSoy: true,
 	testBrowsers: ['Chrome', 'Firefox', 'Safari', 'IE10 - Win7', 'IE11 - Win7'],
 	testSaucelabsBrowsers: {
-		// sl_chrome: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'chrome'
-		// },
-		// sl_safari_8: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'safari',
-		// 	version: '8'
-		// },
-		// sl_safari_9: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'safari',
-		// 	version: '9'
-		// },
-		// sl_firefox: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'firefox'
-		// },
-		// sl_ie_10: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'internet explorer',
-		// 	platform: 'Windows 7',
-		// 	version: '10'
-		// },
-		// sl_ie_11: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'internet explorer',
-		// 	platform: 'Windows 8.1',
-		// 	version: '11'
-		// },
-		// sl_edge_20: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'microsoftedge',
-		// 	platform: 'Windows 10',
-		// 	version: '13'
-		// },
-		// sl_iphone: {
-		// 	base: 'SauceLabs',
-		// 	browserName: 'iphone',
-		// 	platform: 'OS X 10.10',
-		// 	version: '9.2'
-		// },
+		sl_chrome: {
+			base: 'SauceLabs',
+			browserName: 'chrome'
+		},
+		sl_safari_8: {
+			base: 'SauceLabs',
+			browserName: 'safari',
+			version: '8'
+		},
+		sl_safari_9: {
+			base: 'SauceLabs',
+			browserName: 'safari',
+			version: '9'
+		},
+		sl_firefox: {
+			base: 'SauceLabs',
+			browserName: 'firefox'
+		},
+		sl_ie_10: {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 7',
+			version: '10'
+		},
+		sl_ie_11: {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 8.1',
+			version: '11'
+		},
+		sl_edge_20: {
+			base: 'SauceLabs',
+			browserName: 'microsoftedge',
+			platform: 'Windows 10',
+			version: '13'
+		},
+		sl_iphone: {
+			base: 'SauceLabs',
+			browserName: 'iphone',
+			platform: 'OS X 10.10',
+			version: '9.2'
+		},
 		sl_android_4: {
 			base: 'SauceLabs',
 			browserName: 'android',
